0c8b6e937da9c40f60b888650b2efc9ab8bac3ca
lntechfuse <techtitannattawat@gmail.com>
2024-08-08 15:04:45 +0700

    kllm

--- src/components/Reply.tsx
+++ src/components/Reply.tsx
@@ -1,5 +1,35 @@
 "use client";
 
 export default function Reply({ ImagePath, username, replyTitle, likes }) {
-  return <div>your code for Reply component ...</div>;
+  return <div>your code for Reply component ...
+    <div className="d-flex gap-2 my-2 ps-5">
+          <img
+            src={userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+             {username}
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>{replyText}</span>
+            <div className="d-flex align-items-center gap-1">
+              <img 
+              disabled = {likeNum === 0 }
+              src="/like.svg" width={20}>
+              </img>
+              <span
+                style={{ color: "#B0B3B8" }}>
+                {likeNum}
+               </span>
+            </div>
+          </div>
+        </div>
+  </div>;
 }
